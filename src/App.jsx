101fa2fa4d0dbdfd781de8bcb64f1cff38a35609
Franco Buceta <francobuceta95@gmail.com>
2023-05-10 14:18:20 -0300

    add form in grootdraw component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
-import Form from "./components/Form.jsx";
 import GrootDraw from './components/GrootDraw';
 
 function App() {
 
   return (
     <>
-      <Form />
       <GrootDraw />
     </>
   )
--- src/components/GrootDraw.jsx
+++ src/components/GrootDraw.jsx
@@ -1,37 +1,43 @@
+import Form from "./Form.jsx";
+
 const GrootDraw = () => {
     return (
-        <section className="container">
+        <>
+            <section className="container">
 
-            <div className="hair">
-                <div className="hair-1"></div>
-                <div className="hair-2"></div>
-                <div className="hair-3"></div>
-                <div className="hair-4"></div>
-                <div className="hair-5"></div>
-            </div>
+                <div className="hair">
+                    <div className="hair-1"></div>
+                    <div className="hair-2"></div>
+                    <div className="hair-3"></div>
+                    <div className="hair-4"></div>
+                    <div className="hair-5"></div>
+                </div>
 
-            <div className="head">
-                <div className="face">
-                    <div className="eyes">
-                        <div className="eyes_left"></div>
-                        <div className="eyes_right"></div>
-                    </div>
+                <div className="head">
+                    <div className="face">
+                        <div className="eyes">
+                            <div className="eyes_left"></div>
+                            <div className="eyes_right"></div>
+                        </div>
 
-                    <div className="mouth">
-                        <div className="tongue"></div>
+                        <div className="mouth">
+                            <div className="tongue"></div>
+                        </div>
                     </div>
                 </div>
-            </div>
 
-            <div className="neck"></div>
+                <div className="neck"></div>
 
-            <div className="shoulder"></div>
+                <div className="shoulder"></div>
 
-            <div className="body">
-                <div className="left-arm"></div>
-                <div className="right-arm"></div>
-            </div>
-        </section>
+                <div className="body">
+                    <div className="left-arm"></div>
+                    <div className="right-arm"></div>
+                </div>
+            </section>
+
+            <Form />
+        </>
     )
 }
 
